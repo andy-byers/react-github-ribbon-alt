0652d8c49879c67c657f6a95b20f890411410285
andy-byers <andrew.byers@utdallas.edu>
2021-08-09 09:51:10 -0500

    Updates

---
+++ precache-manifest.2fa9b5b017a842b547a54d6c91c249db.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a57e0e28d4300096622ba1001eb84f52",
+    "url": "./index.html"
+  },
+  {
+    "revision": "de273f0dc60628ee3ef5",
+    "url": "./static/css/main.fec2498e.chunk.css"
+  },
+  {
+    "revision": "f0740e59d59231029459",
+    "url": "./static/js/2.c0797f69.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c0797f69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de273f0dc60628ee3ef5",
+    "url": "./static/js/main.a0368c56.chunk.js"
+  },
+  {
+    "revision": "96ef3a26899dfde9183d",
+    "url": "./static/js/runtime-main.c2a2b932.js"
+  }
+]);
